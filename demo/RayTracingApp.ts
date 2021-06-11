4666d755bdf49d27452cc9ac9642bec3fc7b00fe
dtysky <dtysky@outlook.com>
2021-06-11 17:58:06 +0800

    Update

---
+++ demo/RayTracingApp.ts
@@ -0,0 +1,17 @@
+/**
+ * RayTracingApp.ts
+ * 
+ * @Author  : hikaridai(hikaridai@tencent.com)
+ * @Date    : 6/11/2021, 5:56:30 PM
+*/
+import * as H from '../src/index';
+
+export default class RayTracingApp {
+  public async init() {
+
+  }
+
+  public update(dt: number) {
+
+  }
+}
