1f82821fd3ee0b60133999d52e05fe4d2faa02a3
dtysky <dtysky@outlook.com>
2021-09-06 01:16:20 +0800

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './docs'),
     filename: 'main.js',
     publicPath: '/'
   },
